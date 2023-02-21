2324a328818219786da99450a0bdde70164b48ca
Trinh <trinhdo112001@gmail.com>
2023-02-21 14:07:34 +0700

    #1 banner

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,16 +1,20 @@
 <template>
   <Header />
+  <Banner />
   <Footer />
 </template>
 
 <script lang="ts">
 import Footer from "@/components/footer/Footer.vue";
+import Banner from "@/components/home/Banner.vue";
 import Header from "@/components/header/Header.vue";
+
 import { Options, Vue } from "vue-class-component";
 
 @Options({
   components: {
     Header,
+    Banner,
     Footer,
   },
 })
